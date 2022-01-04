2ea1c0a3a52cd4999e9a04a8956c7dc5555c3623
James Crocker <james@crocker.us>
2022-01-03 17:36:46 -0800

    small changes

--- src/main/java/dev/namesareapain/neidatadump/DumpCommand.java
+++ src/main/java/dev/namesareapain/neidatadump/DumpCommand.java
@@ -7,6 +7,7 @@ import net.minecraft.util.ChatComponentText;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 
 import dev.namesareapain.neidatadump.ExportData;
 public class DumpCommand extends CommandBase {
@@ -35,13 +36,49 @@ public class DumpCommand extends CommandBase {
     public boolean isUsernameIndex(String[] args, int i){return false;}
 
     public String getCommandUsage(ICommandSender sender){
-        return "/neidatadump : dumps data";
+        return "/neidatadump [i]tems [r]ecipes [o]redict [a]spects\nEither space seprated words/letters or a single term -iroa";
     }
 
     public void processCommand(ICommandSender sender, String[] args){
-        sender.addChatMessage( new ChatComponentText("This was a triumph"));
-        ExportData.exportRecipes();
-        ExportData.exportItems();
+        ArrayList<String> options = new ArrayList<String>();
+        if(args.length == 0){
+            options.add("recipes");
+            options.add("items");
+            options.add("oredict");
+        } else if(args.length == 1 && args[0].startsWith("-")){
+            options = new ArrayList<String>(Arrays.asList(args[0].split("")));
+            options.remove("0");
+        } else {
+            options = new ArrayList<String>(Arrays.asList(args));
+        }
+        processCommandChecked(sender,options);
     }
+
+    public void processCommandChecked(ICommandSender sender, List<String> argList){
+        if(argList.contains("r") || argList.contains("recipes")){
+            sender.addChatMessage( new ChatComponentText("Exporting Recipes"));
+            System.out.println("Exporting Recipes");
+            ExportData.exportRecipes();
+        }
+        if(argList.contains("i") || argList.contains("items")){
+            sender.addChatMessage( new ChatComponentText("Exporting Items"));
+            System.out.println("Exporting Item List");
+            ExportData.exportItems();
+        }
+        if(argList.contains("a") || argList.contains("aspects")){
+            sender.addChatMessage( new ChatComponentText("Exporting Aspect List (takes ~10-15 min)"));
+            System.out.println("Exporting Aspect List");
+            ExportData.exportAspects();
+        }
+        if(argList.contains("o") || argList.contains("oredict")){
+            sender.addChatMessage( new ChatComponentText("Exporting Ore Dictionary"));
+            System.out.println("Exporting Ore Dictionary");
+            ExportData.exportOreDict();
+        }
+        System.out.println("Done Exporting");
+        sender.addChatMessage( new ChatComponentText("Done Exporting"));
+        sender.addChatMessage( new ChatComponentText("This was a triumph"));
+    }
+    
 
 }
--- src/main/java/dev/namesareapain/neidatadump/ExportData.java
+++ src/main/java/dev/namesareapain/neidatadump/ExportData.java
@@ -2,20 +2,16 @@ package dev.namesareapain.neidatadump;
 
 import codechicken.nei.recipe.GuiCraftingRecipe;
 import codechicken.nei.recipe.ICraftingHandler;
-import codechicken.nei.recipe.TemplateRecipeHandler;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Arrays;
-import java.lang.reflect.Field;
-import java.util.HashSet;
-import codechicken.nei.PositionedStack;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import codechicken.nei.PositionedStack;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
 import net.minecraft.block.Block;
 import cpw.mods.fml.common.registry.GameRegistry;
-import java.io.PrintWriter;
 import java.awt.image.BufferedImage;
 import java.util.Optional;
 import net.minecraft.util.IIcon;
@@ -23,152 +19,109 @@ import javax.imageio.ImageIO;
 import java.util.Base64;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import java.io.ByteArrayOutputStream;
+import dev.namesareapain.neidatadump.MasterItemList;
+import dev.namesareapain.neidatadump.Jsonify;
+import dev.namesareapain.neidatadump.handlerhandler.*;
+import thaumcraft.api.ThaumcraftApiHelper;
+import net.minecraftforge.oredict.OreDictionary;
 
 public class ExportData {
+    
+    public static long ITEM_COUNT = 0;
+
+    public static ArrayList<IHandlerHandler> handlerHandlers(){ 
+        ArrayList<IHandlerHandler> out = new ArrayList<IHandlerHandler>();
+        out.add(new GT_NEI_DefaultHandlerHandler());
+        out.add(new TemplateRecipeHandlerHandler());
+        return out;
+    }
+
     public static void exportRecipes(){
         System.out.println("beginning data dump");
         ArrayList<ICraftingHandler> neihandlers = GuiCraftingRecipe.craftinghandlers;
         JSONArray handlerdumps = new JSONArray();
         for(ICraftingHandler handler : neihandlers){
-            System.out.println(handler.getRecipeName());
-            System.out.println(handler.getClass().getName());
-            try {
-                handlerdumps.put(templateRecipeDump((TemplateRecipeHandler) handler));
-                System.out.println("TemplateRecipeHandler");
-            } catch(Exception e){
-                System.out.println("Not a TemplateRecipeHandler");
+            for( IHandlerHandler handlerHandler : handlerHandlers()){
+                if(handlerHandler.claim(handler)){
+                    try{
+                        handlerdumps.put(handlerHandler.dumpRecipes(handler));
+                    } catch (Exception e){
+                        System.out.println("Failed to process Handler:" +
+                                handler.getRecipeName()
+                                );
+                    }
+                    break;
+                }
             }
         }
         JSONObject out = new JSONObject();
         out.put("handlers",handlerdumps);
-        writeOutput(out,"./recipes.json");
+        Jsonify.writeOutput(out,"./recipes.json");
     }
 
     public static void exportItems(){
         JSONArray out = new JSONArray();
-        for (Object item_obj : Item.itemRegistry){
-            try{
-                Item item = (Item) item_obj;
-                ArrayList<ItemStack> subitems = new ArrayList<ItemStack>();
-                item.getSubItems(item,null,subitems);
-                for(ItemStack subitem : subitems){
-                    ItemStack stack = subitem;
-                    out.put(new JSONObject()
-                        .put("item",jsonifyItem(stack))
-                        .put("name",stack.getDisplayName())
-                    );
-                }
+        long i = 0;
+        for(ItemStack stack : new MasterItemList()){    
+            try {
+                out.put(new JSONObject()
+                    .put("item",Jsonify.item(stack))
+                    .put("name",stack.getDisplayName())
+                );
+                i++;
             } catch(Exception e){
                 System.out.println(e.toString());
             }
         }
-        writeOutput(new JSONObject().put("items",out),"./itemlist.json");
+        Jsonify.writeOutput(new JSONObject().put("items",out),"./itemlist.json");
+        ITEM_COUNT = i;
+        System.out.println("Total items: " + ITEM_COUNT);
     }
-    
-    public static JSONObject templateRecipeDump(TemplateRecipeHandler handler){
-        handler.loadTransferRects();
-        System.out.println("Num. Transfer Rects: " + handler.transferRects.size());
-        HashSet<String> uniqueRects = new HashSet<String>();
-        for(TemplateRecipeHandler.RecipeTransferRect rect : handler.transferRects){
+
+    public static void exportOreDict(){
+        JSONArray orenames = new JSONArray();
+        for(String orename : OreDictionary.getOreNames()){ 
+            JSONArray entries = new JSONArray();
+            for(ItemStack entry : OreDictionary.getOres(orename)){
+                entries.put(Jsonify.item(entry.getItem()));
+            }
+            orenames.put(new JSONObject()
+                            .put("tag",orename)
+                            .put("entries",entries)
+                        );
+        }
+        Jsonify.writeOutput(new JSONObject().put("ore_dictionary",orenames),"./oredictionary.json");
+    }
+
+    public static void exportAspects(){
+        JSONArray out = new JSONArray();
+        long i = 0;
+        long j = 0;
+        for(ItemStack stack : new MasterItemList()){
             try{
-                Field outputIdField = rect.getClass().getDeclaredField("outputId");
-                outputIdField.setAccessible(true);
-                System.out.println("Rect output Id: " + outputIdField.get(rect).toString());
-                uniqueRects.add(outputIdField.get(rect).toString());
-            } catch( Exception e){
-                System.out.println("damn");
+                out.put(new JSONObject()
+                        .put("aspects",Jsonify.aspectList(
+                                ThaumcraftApiHelper.getObjectAspects(stack)
+                                )
+                            )
+                        .put("item",Jsonify.item(stack))
+                        );
+            } catch ( Exception e){
+                System.out.println("Aspect query failed for " + stack.getDisplayName());
+                e.printStackTrace();
+            }
+            i++;
+            if(i > j*(ITEM_COUNT/10)){
+                System.out.println(j + "0% item aspects complete");
+                j++;
             }
         }
-        for(String rect : uniqueRects){
-            handler.loadCraftingRecipes(rect);
-            System.out.println("After loading " + rect + " Num recipes: " + handler.numRecipes());
-        }
-        JSONArray recipes = new JSONArray();
-        for(int i = 0; i < handler.numRecipes(); i++){
-            recipes.put(jsonifyRecipe(handler.getRecipeName(),handler.getIngredientStacks(i),handler.getOtherStacks(i),handler.getResultStack(i)));
-        }
-        JSONObject out = new JSONObject();
-        out.put("recipes", recipes);
-        out.put("handler", handler.getRecipeName());
-        return out;
+        Jsonify.writeOutput(
+                new JSONObject()
+                .put("aspects",out)
+                ,"./aspects.json"
+                );
     }
-
-    public static JSONObject jsonifyRecipe(String handlername, List<PositionedStack> inputs, List<PositionedStack> others, PositionedStack results){
-        return new JSONObject()
-            .put("inputs",jsonifyPositionedStackList(inputs))
-            .put("others",jsonifyPositionedStackList(others))
-            .put("output",jsonifyPositionedStack(results));
-    }
-
-    public static ArrayList<JSONObject> jsonifyPositionedStackList(List<PositionedStack> stacks){
-        ArrayList<JSONObject> out = new ArrayList<JSONObject>();
-        for(PositionedStack stack : stacks){
-            out.add(jsonifyPositionedStack(stack));
-        }
-        return out;
-    }
-
-    public static JSONObject jsonifyPositionedStack(PositionedStack stack){
-        return new JSONObject()
-            .put("relx", stack.relx)
-            .put("rely", stack.rely)
-            .put("items", jsonifyItemStackArray(stack.items));
-    }
-
-    public static ArrayList<JSONObject> jsonifyItemStackArray(ItemStack[] stacks){
-        ArrayList<JSONObject> out = new ArrayList<JSONObject>();
-        for(ItemStack stack : stacks){
-            out.add(jsonifyItemStack(stack));
-        }
-        return out;
-    }
-
-    public static JSONObject jsonifyItemStack(ItemStack stack){
-        return new JSONObject()
-           .put("item", jsonifyItem(stack))      
-           .put("count", stack.stackSize);
-    }
-
-    public static JSONObject jsonifyItem(Item item){
-        return jsonifyItem(new ItemStack(item));
-    }
-
-    public static JSONObject jsonifyItem(Block item){
-        return jsonifyItem(new ItemStack(item));
-    }
-
-    public static JSONObject jsonifyItem(ItemStack stack){
-        return new JSONObject()
-            .put("id", GameRegistry.findUniqueIdentifierFor(stack.getItem()).name)
-            .put("modid", GameRegistry.findUniqueIdentifierFor(stack.getItem()).modId)
-            .put("metadata",stack.getItemDamage());
-
-    }
-
-    public static void writeOutput(JSONObject output,String filename){
-        try {
-            PrintWriter writer = new PrintWriter(filename, "UTF-8");
-            writer.println(output.toString(2));
-            writer.close();
-        } catch (Exception e){System.out.println(e.toString());}
-    }
-
-    public static Optional<String> base64ItemIcon(ItemStack stack){
-        IIcon icon = stack.getItem().getIconFromDamage(stack.getItemDamage());
-        BufferedImage img = new BufferedImage(icon.getIconWidth(),icon.getIconHeight(),BufferedImage.TYPE_4BYTE_ABGR);
-        try{
-            img.setRGB(0,0,icon.getIconWidth(),icon.getIconHeight(),((TextureAtlasSprite) icon).getFrameTextureData(0)[0],0,icon.getIconWidth());
-            return Optional.of(imgToBase64String(img));
-        } catch(Exception e){
-            System.out.println(e.toString());
-            return Optional.empty();
-        }
-    }
-
-    public static String imgToBase64String(BufferedImage img) throws Exception{
-        ByteArrayOutputStream outstream = new ByteArrayOutputStream();
-        ImageIO.write(img,"PNG",outstream);
-        return Base64.getEncoder().encodeToString(outstream.toByteArray());
-    }
-
+    
+    
 }
